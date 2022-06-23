bc8bc0245daaee1198947d0997a6d3aec1d86fcc
Umar <81313320+umarsalihu99@users.noreply.github.com>
2022-06-23 09:33:34 +0100

    Add JS File

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # arithmetic-calculator-nofrontend
+
+This is my solution for the Zuri Training Task.
+
+Herein is the Js file for the Arithmetic Calculator with No Frontend task.
+
+URL = https://umarsalihu99.github.io/arithmetic-calculator-nofrontend/
---
+++ script.js
@@ -0,0 +1,13 @@
+let numb1 = parseInt(prompt("First Number: "));
+let opr = prompt("Operator: ");
+let numb2 = parseInt(prompt("Second Number: "));
+
+if (opr == "+") {
+    alert(numb1 + numb2)
+} else if (opr == "-") {
+    alert(numb1 - numb2)
+} else if (opr == "*") {
+    alert(numb1 * numb2)
+} else if (opr == "/") {
+    alert(numb1 / numb2)
+}
